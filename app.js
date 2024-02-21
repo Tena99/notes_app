62e7d8944822b6125bdea371477bb3e600c85992
Anastasiia Volvis <stacyvolvis@gmail.com>
2024-02-21 17:50:51 +0100

    DB setup

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ let express = require("express");
 let app = express();
 const port = 3000;
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Welcome to our note-taking app!" });
+let postgres = require("@vercel/postgres");
+
+app.get("/", async (request, response) => {
+  createNotes();
+  const { rows } = await postgres.sql`SELECT * FROM notes`;
+
+  return response.json(rows);
 });
 
 app.get("*", (request, response) => {
   return response.status(404).json({ message: "Sorry. Page not found" });
 });
+
+async function createNotes() {
+  await postgres.sql`CREATE TABLE IF NOT EXISTS notes (
+    id SERIAL PRIMARY KEY,
+      content VARCHAR(255)
+    ) `;
+}
 
 module.exports = app;
