daf3dacf20d18b96e9227f969bdc68e1bfe2cf83
Anastasiia Volvis <stacyvolvis@gmail.com>
2024-02-22 14:33:53 +0100

    Add user and notes per user

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 let express = require("express");
 let app = express();
-const port = 3000;
 
 let postgres = require("@vercel/postgres");
 
 app.get("/", async (request, response) => {
-  createNotes();
+  createTables();
   const { rows } = await postgres.sql`SELECT * FROM notes`;
+
+  return response.json(rows);
+});
+
+app.get("/:user", async (request, response) => {
+  createTables();
+  const { user } = request.params;
+
+  const { rows } =
+    await postgres.sql`SELECT * FROM notes INNER JOIN users ON notes."userID" = users.id WHERE users.name = ${user}`;
+
+  return response.json(rows);
+});
+
+app.get("/:user/:noteID", async (request, response) => {
+  createTables();
+
+  const { user, noteID } = request.params;
+
+  const { rows } =
+    await postgres.sql`SELECT * FROM notes INNER JOIN users on notes."userID" = users.id WHERE notes.id = ${noteID} AND users.name = ${user}`;
 
   return response.json(rows);
 });
@@ -15,10 +35,16 @@ app.get("*", (request, response) => {
   return response.status(404).json({ message: "Sorry. Page not found" });
 });
 
-async function createNotes() {
+async function createTables() {
+  await postgres.sql`CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY, 
+      name VARCHAR(255)
+)`;
+
   await postgres.sql`CREATE TABLE IF NOT EXISTS notes (
-    id SERIAL PRIMARY KEY,
-      content VARCHAR(255)
+      id SERIAL PRIMARY KEY,
+      content VARCHAR(255),
+      "userID" INTEGER REFERENCES users(id)
     ) `;
 }
 
